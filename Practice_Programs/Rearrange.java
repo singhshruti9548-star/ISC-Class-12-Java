a272a3a11f538a82282fb63066a2621c8c84cbea
Shruti <singhshruti9548@gmail.com>
2026-01-01 18:55:23 +0530

    Update Rearrange.java

--- Practice_Programs/Rearrange.java
+++ Practice_Programs/Rearrange.java
@@ -1,3 +1,5 @@
+
+
 import java.util.*;
 class Rearrange
 {
@@ -15,5 +17,49 @@ class Rearrange
         System.out.println("Enter a word in UpperCase=");
         Txt=sc.next().toUpperCase();
         len=Txt.length();
+    }
+    void convert()
+    {
+        int fl=0;
+        char s = Txt.charAt(0);
+        
+        if(s=='A'||s=='E'||s=='I'||s=='O'||s=='U')
+        {
+        Cxt= Txt+ "Y";
+        return;//fl=1; (this could also be written)
+        }
+        else   
+        if(fl==0)
+      {
+         for(int i=0; i<len;i++)
+         {
+             char w = Txt.charAt(i);
+             if(w=='A'||w=='E'||w=='I'||w=='O'||w=='U')
+             {
+                 fl=1;
+                 String s1 = Txt.substring(i,len);
+                 String s2 = Txt.substring(0,i);
+                 Cxt= s1+s2+"C";
+                 break;
+             }
+         }
+      }
+          
+          if(fl==0)
+          {
+          Cxt=Txt+"N";
+          }
+    }
+    void display()
+    {
+        System.out.println("the original word is="+Txt);
+        System.out.println("the changed word is="+Cxt);
+    }
+    public static void main(String args[])
+    {
+        Rearrange obj= new Rearrange();
+        obj.readword();
+        obj.convert();
+        obj.display();
     }
 }
