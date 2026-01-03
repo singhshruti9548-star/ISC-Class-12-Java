2d7c0fddc417b19f7de73558f559ee9e8e15472b
Shruti <singhshruti9548@gmail.com>
2026-01-03 12:15:16 +0530

    Create  SymmetricMatrix.java

---
+++ Practice_Programs/SymmetricMatrix.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class SymmetricMatrix
+{
+  public static void main(String args[])
+  {
+      int n, i,j,fl=0;
+      Scanner sc= new Scanner (System.in);
+      System.out.println("Enter size ");
+      
+  }
+}
