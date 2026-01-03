d66eae34a53d2f87118514df804190fc647aadfc
Shruti <singhshruti9548@gmail.com>
2026-01-03 22:12:34 +0530

    Create Change.java

---
+++ Practice_Programs/Change.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Change
+{
+    String str, newstr;
+    int len;
+    Change()
+    {
+        str= " ";
+        newstr= " ";
+        len = 0;
+    }
+    void inputword()
+    {
+        Scanner sc= new Scanner (System.in);
+        System.out.println(" enter a word");
+        word= sc.next();
+        
+    }
+    
+}
