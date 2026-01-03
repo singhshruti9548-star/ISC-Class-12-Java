7ad0f086d44363dad5bffde712b6bb6ffde5803d
Shruti <singhshruti9548@gmail.com>
2026-01-03 13:07:46 +0530

    Update SymmetricMatrix.java

--- Practice_Programs/SymmetricMatrix.java
+++ Practice_Programs/SymmetricMatrix.java
@@ -1,3 +1,4 @@
+
 import java.util.*;
 class SymmetricMatrix
 {
@@ -5,7 +6,31 @@ class SymmetricMatrix
   {
       int n, i,j,fl=0;
       Scanner sc= new Scanner (System.in);
-      System.out.println("Enter size ");
-      
-  }
-}
+      System.out.println("Enter size ");// taking size from user
+      n= sc.nextInt();
+     int  arr[][]=new int[n][n];// block creation
+      for (i=0;i<n; i++)
+      {                    // taking array element 
+          for (j= 0; j<n ; j++)
+          {
+              System.out.println(" enter array element");
+              arr[i][j] = sc.nextInt();
+          }
+      }
+      for (i=0;i<n; i++)
+      {                    // taking array element 
+          for (j= 0; j<n ; j++)
+          {
+              if ( arr[i][j] != arr[j][i])
+              { 
+                  fl= 1;
+                  break;
+              }
+         }
+     }
+     if (fl==1)
+     System.out.println(" not a symmetric matrix");
+     else
+     System.out.println("  a symmetric matrix");
+   }
+} 
