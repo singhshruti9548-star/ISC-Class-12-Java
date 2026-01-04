b54c6a951ef502dc3a2b4b4f841f18d6e021d96b
Shruti <singhshruti9548@gmail.com>
2026-01-04 10:56:06 +0530

    Update Change.java

--- Practice_Programs/Change.java
+++ Practice_Programs/Change.java
@@ -17,4 +17,44 @@ class Change
         
     }
     
-}
+}import java.util.*;
+class Change
+{
+    String str, newstr;
+    int len;
+    Change()
+    {
+        str= " ";
+        newstr= " ";
+        len = 0;
+    }
+    void inputword()
+    {
+        Scanner sc= new Scanner (System.in);
+        System.out.println(" enter a word");
+        str = sc.next();
+        len = str.length();
+    }
+    char caseconvert(char ch)
+    {
+        for(int i=0; i<len; i++ )
+        {
+            ch= str.charAt(i);
+            if(ch>= 'A' && ch <= 'Z')
+            {
+            char w = Character.toLowerCase(ch);
+            return w;
+            }
+            else
+            if(ch>= 'a' && ch <= 'z')
+            {
+            char x= Character.toUpperCase(ch);
+            return x;
+            }
+        }
+    }
+    void recchange()
+    {
+        
+    }
+
