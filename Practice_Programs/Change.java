8a6351071bda4d1dc6b0285cfb86c7a340eb9434
Shruti <singhshruti9548@gmail.com>
2026-01-05 12:40:30 +0530

    Update Change.java

--- Practice_Programs/Change.java
+++ Practice_Programs/Change.java
@@ -13,48 +13,47 @@ class Change
     {
         Scanner sc= new Scanner (System.in);
         System.out.println(" enter a word");
-        word= sc.next();
-        
-    }
-    
-}import java.util.*;
-class Change
-{
-    String str, newstr;
-    int len;
-    Change()
-    {
-        str= " ";
-        newstr= " ";
-        len = 0;
-    }
-    void inputword()
-    {
-        Scanner sc= new Scanner (System.in);
-        System.out.println(" enter a word");
         str = sc.next();
         len = str.length();
     }
     char caseconvert(char ch)
     {
-        for(int i=0; i<len; i++ )
-        {
-            ch= str.charAt(i);
-            if(ch>= 'A' && ch <= 'Z')
+            if(ch>= 65 && ch <= 90)// implicit type promotion(char to int)
             {
-            char w = Character.toLowerCase(ch);
-            return w;
+            char w = Character.toLowerCase(ch);// case conversion by 
+            return w;                          //Wrapper class
             }
             else
             if(ch>= 'a' && ch <= 'z')
             {
-            char x= Character.toUpperCase(ch);
+            char x= (char) (ch-32);// case conversion via type Casting
             return x;
             }
         }
     }
-    void recchange()
+    void recchange(int pos)
     {
-        
+        char ch;
+        if(pos>-1)
+        {
+            ch= str.charAt(pos);
+            ch= caseconvert(ch);
+            newstr = newstr+ recchange(pos-1);
+        }
     }
+    void display()
+    {
+        System.out.println(" the original word =" + str);
+        System.out.println(" the new  word ="+ newstr);
+    }
+    public static void main(String args[])
+    {
+        Change obj = new Change();
+        obj.inputword();
+        obj.recchange(len-1);
+        obj.display();
+    }
+}
 
+
+    
